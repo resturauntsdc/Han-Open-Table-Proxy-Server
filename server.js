2b8d26ae48bb509109556f7be8347b6b6a13a124
Han Lin Aung <hanlinaunghack@gmail.com>
2018-10-24 17:42:59 -0700

    skeleton setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const proxy = require("http-proxy-middleware");
+const parser = require("body-parser");
+
+app.use(express());
+app.use(parser.json());
+app.use(express.static("public"));
+
+app.use("/", proxy({ target: "http://www.example.org", changeOrigin: true }));
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
