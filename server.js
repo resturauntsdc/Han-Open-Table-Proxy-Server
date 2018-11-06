a6783b72af4499d63c5bba760500ebd29725229a
Han Lin Aung <hanlinaunghack@gmail.com>
2018-11-06 08:47:18 -0800

    some fixes

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const proxy = require("http-proxy-middleware");
 const path = require("path");
 const parser = require("body-parser");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 
 app.set("port", process.env.PORT || 3000);
 
@@ -23,15 +25,47 @@ app.get("/", function(req, res) {
 //   changeOrigin: true
 // };
 
+app.use(
+  "/restaurants/:restaurantID/menu/:menu",
+  proxy({ target: "http://127.0.0.1:3001/", changeOrigin: true })
+);
 app.get("/restaurants/:restaurantID/menu/:menu", function(req, res) {
   const menuPath = path.join(__dirname, "./public/index.html");
   res.sendFile(menuPath);
 });
 
 app.use(
-  "api/restaurants/:restaurantID/menu/:menu",
-  proxy({ target: "http://127.0.0.1:3001/" })
+  "/restaurants/:rest_id/gallery",
+  proxy({ target: "http://127.0.0.1:3004/", changeOrigin: true })
 );
+app.get("/restaurants/:rest_id/gallery", function(req, res) {
+  const menuPath = path.join(__dirname, "./public/index.html");
+  res.sendFile(menuPath);
+});
+
+app.use(
+  "/restaurants/:restaurantid/reviews",
+  proxy({
+    target: "http://127.0.0.1:3005/"
+  })
+);
+app.use(
+  "/restaurants/:restaurantid/reviewsummary",
+  proxy({
+    target: "http://127.0.0.1:3005/"
+  })
+);
+
+app.get("/restaurants/:restaurantid/reviews", function(req, res) {
+  const menuPath = path.join(__dirname, "./public/index.html");
+  res.sendFile(menuPath);
+});
+app.get("/restaurants/:restaurantid/reviewsummary", function(req, res) {
+  const menuPath = path.join(__dirname, "./public/index.html");
+  res.sendFile(menuPath);
+});
+
+//shouldn't this come first?
 
 // app.use("api/restaurants/:restaurantid/reviewsummary", proxy(reviewsummary));
 
