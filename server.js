7bfd71536aea65840623a6bd6c4d0d9cb3955b24
Han Lin Aung <hanlinaunghack@gmail.com>
2018-11-10 12:07:14 -0800

    fixed

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ app.get("/", function(req, res) {
 
 app.use(
   "/restaurants/:restaurantID/menu/:menu",
-  proxy({ target: "http://127.0.0.1:3001/", changeOrigin: true })
+  proxy({ target: "http://54.215.211.244/", changeOrigin: true })
 );
 app.use(
   "/restaurants/:restaurantID/menu/special",
-  proxy({ target: "http://127.0.0.1:3001/", changeOrigin: true })
+  proxy({ target: "http://54.215.211.244/", changeOrigin: true })
 );
 app.use(
   "/restaurants/:restaurantID/menu/menuCount",
-  proxy({ target: "http://127.0.0.1:3001/", changeOrigin: true })
+  proxy({ target: "http://54.215.211.244/", changeOrigin: true })
 );
 
 app.get("/restaurants/:restaurantID/menu/:menu", function(req, res) {
@@ -42,54 +42,54 @@ app.get("/restaurants/:restaurantID/menu/menuCount", function(req, res) {
   res.sendFile(menuPath);
 });
 
-app.use(
-  "/restaurants/:id/overview/",
-  proxy({ target: "http://127.0.0.1:3002/", changeOrigin: true })
-);
-app.get("/restaurants/:id/overview/", function(req, res) {
-  const overview = path.join(__dirname, "./public/index.html");
-  res.sendFile(overview);
-});
+// app.use(
+//   "/restaurants/:id/overview/",
+//   proxy({ target: "http://127.0.0.1:3002/", changeOrigin: true })
+// );
+// app.get("/restaurants/:id/overview/", function(req, res) {
+//   const overview = path.join(__dirname, "./public/index.html");
+//   res.sendFile(overview);
+// });
 
-app.use(
-  "/restaurants/:restaurantID/reservations",
-  proxy({ target: "http://127.0.0.1:3003/", changeOrigin: true })
-);
-app.get("/restaurants/:restaurantID/reservations", function(req, res) {
-  const reservations = path.join(__dirname, "./public/index.html");
-  res.sendFile(reservations);
-});
+// app.use(
+//   "/restaurants/:restaurantID/reservations",
+//   proxy({ target: "http://127.0.0.1:3003/", changeOrigin: true })
+// );
+// app.get("/restaurants/:restaurantID/reservations", function(req, res) {
+//   const reservations = path.join(__dirname, "./public/index.html");
+//   res.sendFile(reservations);
+// });
 
-app.use(
-  "/restaurants/:rest_id/gallery",
-  proxy({ target: "http://127.0.0.1:3004/", changeOrigin: true })
-);
-app.get("/restaurants/:rest_id/gallery", function(req, res) {
-  const gallery = path.join(__dirname, "./public/index.html");
-  res.sendFile(gallery);
-});
+// app.use(
+//   "/restaurants/:rest_id/gallery",
+//   proxy({ target: "http://127.0.0.1:3004/", changeOrigin: true })
+// );
+// app.get("/restaurants/:rest_id/gallery", function(req, res) {
+//   const gallery = path.join(__dirname, "./public/index.html");
+//   res.sendFile(gallery);
+// });
 
-app.use(
-  "/restaurants/:restaurantid/reviews",
-  proxy({
-    target: "http://127.0.0.1:3005/"
-  })
-);
-app.use(
-  "/restaurants/:restaurantid/reviewsummary",
-  proxy({
-    target: "http://127.0.0.1:3005/"
-  })
-);
+// app.use(
+//   "/restaurants/:restaurantid/reviews",
+//   proxy({
+//     target: "http://127.0.0.1:3005/"
+//   })
+// );
+// app.use(
+//   "/restaurants/:restaurantid/reviewsummary",
+//   proxy({
+//     target: "http://127.0.0.1:3005/"
+//   })
+// );
 
-app.get("/restaurants/:restaurantid/reviews", function(req, res) {
-  const reviews = path.join(__dirname, "./public/index.html");
-  res.sendFile(reviews);
-});
-app.get("/restaurants/:restaurantid/reviewsummary", function(req, res) {
-  const reviews = path.join(__dirname, "./public/index.html");
-  res.sendFile(reviews);
-});
+// app.get("/restaurants/:restaurantid/reviews", function(req, res) {
+//   const reviews = path.join(__dirname, "./public/index.html");
+//   res.sendFile(reviews);
+// });
+// app.get("/restaurants/:restaurantid/reviewsummary", function(req, res) {
+//   const reviews = path.join(__dirname, "./public/index.html");
+//   res.sendFile(reviews);
+// });
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
